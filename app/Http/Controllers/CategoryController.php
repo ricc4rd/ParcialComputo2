bc49140c3707bf5ae7ba8a9ce93570dd4cf3bfd7
ricc4rd <menjivarsoto09@gmail.com>
2022-04-23 22:58:01 -0600

    agregar peticion en el CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Resources\Category as CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -13,7 +15,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        // get all categories peticion
+        $categories = Category::all();
+        return CategoryResource::collection($categories);
     }
 
     /**
